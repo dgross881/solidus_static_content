3e8d57f183c53dfa08794e294c27fece7d2e0270
dgross881 <dgross881@gmail.com>
2015-11-16 12:15:53 +0800

    removed pages sidebar menu

--- app/overrides/add_pages_to_admin_sidebar_menu.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'pages_admin_sidebar_menu',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/pages_sidebar_menu'
-)
