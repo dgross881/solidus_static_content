b428642535a5972649f1bc3e46cdeaa83c969a40
dgross881 <dgross881@gmail.com>
2016-01-18 17:08:20 +0800

    removed store from page show

--- app/controllers/spree/pages_controller.rb
+++ app/controllers/spree/pages_controller.rb
@@ -6,7 +6,7 @@ module Spree
     layout :determine_layout
 
     def show
-      @page = Spree::Page.by_store(current_store).visible.friendly.find_by_slug!(params[:id])
+      @page = Spree::Page.visible.friendly.find_by_slug!(params[:id])
     end
 
     private
